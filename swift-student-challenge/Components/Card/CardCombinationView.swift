c96cbaacb113538b296e6ccba38049a8cfcb2304
13ry4nV3rn4nd4 <bryan.vernanda@binus.ac.id>
2025-01-18 14:20:48 +0700

    fix: CardCombinationView to handle odd number cases

--- swift-student-challenge/Components/Card/CardCombinationView.swift
+++ swift-student-challenge/Components/Card/CardCombinationView.swift
@@ -17,9 +17,19 @@ struct CardCombinationView: View {
     
     var body: some View {
         ScrollView {
-            LazyVGrid(columns: [GridItem(.flexible()), GridItem(.flexible())], spacing: 20) {
-                ForEach(patternData) { data in
-                    CardSpinView(patternData: data)
+            VStack(spacing: 20) {
+                // Display items in a 2-column grid
+                LazyVGrid(columns: [GridItem(.flexible()), GridItem(.flexible())], spacing: 20) {
+                    ForEach(patternData.prefix(patternData.count - (patternData.count % 2))) { data in
+                        CardSpinView(patternData: data)
+                    }
+                }
+                
+                // Center the last item if the count is odd
+                if patternData.count % 2 != 0 {
+                    let lastItem = patternData[patternData.count - 1]
+                    CardSpinView(patternData: lastItem)
+                        .frame(maxWidth: .infinity, alignment: .center) // Center the last item
                 }
             }
             .padding()
