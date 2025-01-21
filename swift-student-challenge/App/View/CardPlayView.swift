20d45925ca25bc6f07ce25d0aa387743e7da734e
13ry4nV3rn4nd4 <bryan.vernanda@binus.ac.id>
2025-01-21 14:47:44 +0700

    feat: integrate UI for the main gameplay view

--- swift-student-challenge/App/View/CardPlayView.swift
+++ swift-student-challenge/App/View/CardPlayView.swift
@@ -8,56 +8,55 @@
 import SwiftUI
 
 struct CardPlayView: View {
-    @StateObject private var viewModel = MainViewModel()
+    @StateObject private var viewModel = CardPlayViewModel()
     
     var body: some View {
-        VStack {
-            Text("Level: \(viewModel.level)")
-                .foregroundStyle(.white)
+        ZStack {
+            Color.black
+                .ignoresSafeArea()
             
-            CardCombinationView(patternData: viewModel.patterns, time: viewModel.time) {
-                viewModel.isAnimationRunning = false
-            }
-            .id("CardCombinationView-\(viewModel.level)")
-            
-            if let index = viewModel.patterns.firstIndex(where: { !$0.isUnlocked }) {
-                PatternInputView(
-                    requiredPattern: viewModel.patterns[index].path
-                ) { status, pattern in
-                    if status {
-                        withAnimation(.spring(response: 0.45)) {
-                            viewModel.patterns[index].isUnlocked = true
+            VStack {
+                Text("Level: \(viewModel.level)")
+                    .foregroundStyle(.white)
+                
+                CardCombinationView(patternData: viewModel.patterns, time: viewModel.time) {
+                    viewModel.isAnimationRunning = false
+                }
+                .id("CardCombinationView-\(viewModel.level)")
+                
+                if let index = viewModel.patterns.firstIndex(where: { !$0.isUnlocked }) {
+                    PatternInputView(
+                        requiredPattern: viewModel.patterns[index].path
+                    ) { status, pattern in
+                        if status {
+                            withAnimation(.spring(response: 0.45)) {
+                                viewModel.patterns[index].isUnlocked = true
+                            }
                         }
                     }
-                }
-                .disabled(viewModel.isAnimationRunning)
-            } else {
-                Button(action: {
-                    viewModel.goToNextLevel()
-                    viewModel.isAnimationRunning = true
-                }) {
-                    Text("Next Level")
-                        .font(.headline)
-                        .padding()
-                        .background(Color.blue)
-                        .foregroundColor(.white)
-                        .cornerRadius(10)
+                    .disabled(viewModel.isAnimationRunning)
+                } else {
+                    Button(action: {
+                        viewModel.goToNextLevel()
+                        viewModel.isAnimationRunning = true
+                    }) {
+                        Text("Next Level")
+                            .font(.headline)
+                            .padding()
+                            .background(Color.blue)
+                            .foregroundColor(.white)
+                            .cornerRadius(10)
+                    }
                 }
             }
         }
-        .background(.black)
         .onAppear {
-            viewModel.resetLevel()
-            viewModel.newLevelSetup()
+            viewModel.loadLevel()
         }
+        .navigationBarBackButtonHidden(true)
+            
     }
 }
-
-//    .onChange(of: viewModel.patterns) { _,newValue in
-//        if newValue.allSatisfy({ $0.isUnlocked }) {
-//            viewModel.goToNextLevel()
-//        }
-//    }
 
 #Preview {
     CardPlayView()
